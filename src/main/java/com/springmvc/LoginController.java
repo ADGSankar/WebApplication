c21d8cc5ed786009864ec9624a899a218cf97207
A.D.G.Sankar <a.d.g.sankar@gmail.com>
2019-03-13 13:16:27 +0530

    created basic views in spring mvc

--- src/main/java/com/springmvc/LoginController.java
+++ src/main/java/com/springmvc/LoginController.java
@@ -2,16 +2,15 @@ package com.springmvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {  
 	
 
 	@RequestMapping(value="/login")
-	@ResponseBody
 	public String sayHello()
 	{
-		return "Hello world! this is sankar";
+		return "login";
+//		return "/WEB-INF/views/"+"welcome"+".jsp"; we can do this also without adding a bean to append /WEB-INF/views/ as prefix and .jsp as suffix
 	}
 }
