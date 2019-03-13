4f8ef8b4f7c74f9799c910876bc1c207a551aadf
A.D.G.Sankar <a.d.g.sankar@gmail.com>
2019-03-13 16:24:37 +0530

    added basic authentication to the application for checking username and password

--- src/main/java/com/springmvc/LoginController.java
+++ src/main/java/com/springmvc/LoginController.java
@@ -6,10 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.first_webapp.UserValidationService;
+
 @Controller
 public class LoginController {  
 	
-
+	UserValidationService checkUser=new UserValidationService();
+	
 	@RequestMapping(value="/login",method=RequestMethod.GET )
 	public String showLoginPage()
 	{
@@ -19,9 +22,17 @@ public class LoginController {
 	@RequestMapping(value="/login",method=RequestMethod.POST )
 	public String handleLoginRequest(@RequestParam String name,@RequestParam String password,ModelMap model)
 	{
-		model.put("pname",name);
-		model.put("password",password);
-		return "welcome";
-//		return "/WEB-INF/views/"+"welcome"+".jsp"; we can do this also without adding a bean to append /WEB-INF/views/ as prefix and .jsp as suffix
+			if(!checkUser.isUserValid(name, password))
+			{
+				model.put("errorMsg", "Invalid Credentials");
+				return "login";
+			}
+			else
+			{
+				model.put("pname",name);
+				model.put("password",password);
+				return "welcome";
+//				return "/WEB-INF/views/"+"welcome"+".jsp"; we can do this also without adding a bean to append /WEB-INF/views/ as prefix and .jsp as suffix
+			}
 	}
 }
